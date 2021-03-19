ca93bee0ebaddf47de8f6d268b1884ff133971d1
Amarvir <amarvirgill06@gmail.com>
2021-03-19 13:52:25 -0400

    done

--- js/main.js
+++ js/main.js
@@ -1,29 +1,3 @@
-$(document).ready(function () {
-    if (sessionStorage.getItem('SessionName') != ' ') {
-        document.getElementById("login-user").innerHTML = sessionStorage.getItem('SessionName');
-        document.getElementById("logged-sign-in").style.display = "none";
-        document.getElementById("logged-in").style.display = "none";
-    } else {
-        document.getElementById("login-user").style.display = "none";
-        document.getElementById("logoutId").style.display = "none";
-        document.getElementById("wishlist-page").style.display = "none";
-
-    }
-
-});
-
-function addToCart(elem) {
-
-    var productName = document.getElementById("product-price-" + elem).innerHTML;
-    var addCard = '<div class="productCard" id="product-price-' + elem + '" >' + productName + '</div > ';
-    var getFavoriteData = localStorage.getItem('cart');
-    if (getFavoriteData !== null && getFavoriteData !== '') {
-        getFavoriteData += addCard;
-    } else {
-        getFavoriteData = addCard;
-    }
-    localStorage.setItem('cart', getFavoriteData);
-}
 
 
 $(document).ready(function () {
@@ -95,6 +69,21 @@ function SignUp() {
     localStorage.setItem(data.Email, JSON.stringify(data));
     alert('Registered Successfully.Please login now');
     window.location.href = "signIn.html";
+}
+
+function cancel() {
+    alert("Order cancelled");
+    window.location.href = "index.html";
+    // window.history.back();
+  }
+
+function placed(){
+
+    alert("Order placed successfully");
+    clearCart();
+    window.history.back();
+
+
 }
 
 $(function () {
